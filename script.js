2669d1bd43cd245233617f0cf134da955877a47c
Rob Bowman <robertbowman17@btinternet.com>
2020-02-26 20:24:42 +0000

    Move small screen navbar to the bottom.

--- script.js
+++
@@ -1,9 +0,0 @@
-function navMenu() {
-  document.getElementById("header").classList.toggle("auto");
-  document.getElementById("hamburgerMenu").classList.toggle("change");
-  document.getElementById("menu").classList.toggle("show");
-}
-
-function resetNavigation() {
-  document.getElementById("menu").classList.remove("show");
-}
